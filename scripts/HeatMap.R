d2b5d260988920b5a60f2c4f4b15d654cefb26a9
Alexander Lucaci <43097993+aglucaci@users.noreply.github.com>
2021-01-24 20:40:24 -0500

    Add files via upload
    cleaned up, added a way to parse seq names to species names

--- scripts/HeatMap.R
+++ scripts/HeatMap.R
@@ -1,47 +1,135 @@
-#http://rstudio-pubs-static.s3.amazonaws.com/267810_a987a37141024db6a24078fdd6be18e8.html
+## Use this script to analyze both the codon and protein alignments for BDNF.
+## Reference: http://rstudio-pubs-static.s3.amazonaws.com/267810_a987a37141024db6a24078fdd6be18e8.html
 
-setwd("~/Documents/EvolutionOfNeurotrophins/September_2020")
-install.packages("seqinr")
+## First set working directory, manually.
+#setwd("~/Documents/EvolutionOfNeurotrophins/September_2020")
+setwd("~/Documents/BDNF/2021/EvolutionOfNeurotrophins")
 
+## Installers, this library is important for functionality of this script.
+#install.packages("seqinr")
+#install.packages("gplots")
+
+## Load the libraries.
 library(seqinr)
+library("RColorBrewer")
+library("gplots")
 
+## Declares
 #seqs <- read.alignment("~/Downloads/alignment_real.fa", format = "fasta")
-seqs <- read.alignment("MACSE2_Out_NT.fas", format = "fasta", whole.header = TRUE)
+#seqs <- read.alignment("MACSE2_Out_NT.fas", format = "fasta", whole.header = TRUE)
+# Load the Alignments
+seqs <- read.alignment("analysis/MACSE2_Out_AA.fas", format = "fasta", whole.header = TRUE)
 
+## Process the sequence names
 #seqs$nam <- n1[,1]
-names = seqs$nam
-x = strsplit(names, " ")
+#names = seqs$nam
+seqs$nam
+for (i in 1:length(seqs$nam)) {
+  val = seqs$nam[i]
+  print(val)
+  first = strsplit(val, " ")[[1]][2]
+  second = strsplit(val, " ")[[1]][3]
+  
+  if (first == "PREDICTED:") 
+  {
+    first = strsplit(val, " ")[[1]][3]
+    second = strsplit(val, " ")[[1]][4]
+  }
+  #print(c(strsplit(val, " ")[[1]][2], strsplit(val, " ")[[1]][3]) )
+  #print(paste(first, second))
+  seqs$nam[i] = paste(first, second)
+}
+
+
+#for (val in seqs$nam) {
+#for (i in 1:length(seqs$nam)) {
+#  first = strsplit(val, " ")[[1]][2]
+#  second = strsplit(val, " ")[[1]][3]
+  
+#  if (first == "PREDICTED:") 
+#  {
+#    first = strsplit(val, " ")[[1]][3]
+#    second = strsplit(val, " ")[[1]][4]
+#  }
+  #print(c(strsplit(val, " ")[[1]][2], strsplit(val, " ")[[1]][3]) )
+  #print(paste(first, second))
+#  seqs$nam[i] = paste(first, second)
+#}
+
+#seqs$nam <- 
+
+#x = strsplit(names, " ")
 #seqs$nam <- n1[,1]
-x[1:2]
+#x[1:2]
 
 aa <- dist.alignment(seqs, matrix = "similarity")
 aa1 <- as.matrix(aa)
-aa1
 
-range(aa1) 
+#aa1
+#range(aa1) 
 
 
-heatmap(aa1, cexRow = 0.5, cexCol = 0.5)
+#heatmap(aa1, cexRow = 0.5, cexCol = 0.5)
 # Use 'scale' to normalize
 #heatmap(data, scale="column")
-heatmap(aa1, scale="column")
-heatmap(aa1, cexRow = 0.5, cexCol = 0.5, Rowv = NA, Colv = NA)
-aa2 <- aa1[1:7, 8:12]
-heatmap(aa2, cexRow = 0.5, cexCol = 0.5)
+#heatmap(aa1, scale="column")
+
+#heatmap(aa1, cexRow = 0.5, cexCol = 0.5, Rowv = NA, Colv = NA)
+
+#aa2 <- aa1[1:7, 8:12]
+#heatmap(aa2, cexRow = 0.5, cexCol = 0.5)
 
 #https://www.datanovia.com/en/lessons/heatmap-in-r-static-and-interactive-visualization/
 
 # Use RColorBrewer color palette names
-library("RColorBrewer")
-col <- colorRampPalette(brewer.pal(10, "RdYlBu"))(350)
-heatmap(aa1, scale = "none", col =  col, 
-        RowSideColors = rep(c("blue", "pink"), each = 16),
-        ColSideColors = c(rep("purple", 5), rep("orange", 6)))
+#library("RColorBrewer")
+#col <- colorRampPalette(brewer.pal(10, "RdYlBu"))(350)
+#heatmap(aa1, scale = "none", col =  col, 
+#        RowSideColors = rep(c("blue", "pink"), each = 16),
+#        ColSideColors = c(rep("purple", 5), rep("orange", 6)))
 
-install.packages("gplots")
-library("gplots")
-heatmap.2(aa1, scale = "none", col = bluered(100), 
-          trace = "none", density.info = "none")
+#install.packages("gplots")
+#library("gplots")
+
+#heatmap.2(aa1, scale = "none", col = bluered(400), 
+#          trace = "none", density.info = "none")
+
+
+## Plotting
+
+#heatmap.2(aa1, scale = "none", col = bluered(400), cexRow = 0.5, cexCol = 0.5,
+#          trace = "none", density.info = "none", reorderfun=function(d, w) reorder(d, w, agglo.FUN = mean) )
+
+#heatmap.2(x=aa1, 
+#          dendrogram="row",
+#          scale="none",
+#          col="bluered",
+#          trace="none",
+#          labCol=FALSE)
+
+# Default for now
+# https://www.rdocumentation.org/packages/gplots/versions/3.1.1/topics/heatmap.2
+# https://biocorecrg.github.io/CRG_RIntroduction/heatmap-2-function-from-gplots-package.html
+#density info can be histogram or density or none
+
+heatmap.2(x=aa1, 
+          Colv=FALSE, 
+          density.info="histogram",
+          dendrogram="row",
+          col="bluered",
+          trace="none",
+          labCol=FALSE,
+          cexRow=1,cexCol=1,margins=c(12,8))
+
+# END OF FILE
+
+
+
+
+exit <- function() {
+  .Internal(.invokeRestart(list(NULL, NULL), NULL))
+}
+exit()
 
 # #########################################################################################
 # Codon Alignment
@@ -72,6 +160,7 @@ protein_seqs <- read.alignment("MACSE2_Out_AA.msa", format = "fasta")
 protein_aa <- dist.alignment(protein_seqs, matrix = "similarity")
 protein_aa1 <- as.matrix(protein_aa)
 library("gplots")
+
 heatmap.2(protein_aa1, scale = "none", col = bluered(100), 
           trace = "none", density.info = "none")
 heatmap.2(protein_aa1, scale = "none", col = bluered(100), cexRow = 0.5, cexCol = 0.5,
@@ -79,6 +168,10 @@ heatmap.2(protein_aa1, scale = "none", col = bluered(100), cexRow = 0.5, cexCol 
 
 heatmap.2(protein_aa1, scale = "none", col = bluered(100), cexRow = 0.5, cexCol = 0.5,
           trace = "none", density.info = "none", reorderfun=function(d, w) reorder(d, w, agglo.FUN = mean) )
+
+heatmap.2(aa1, scale = "none", col = bluered(400), cexRow = 0.5, cexCol = 0.5,
+          trace = "none", density.info = "none", reorderfun=function(d, w) reorder(d, w, agglo.FUN = mean) )
+
 
 # #########################################################################################
 
