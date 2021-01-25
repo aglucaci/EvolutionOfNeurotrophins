60da720cfed1d3654e1088346c0973e9df8e57ae
Alexander Lucaci <43097993+aglucaci@users.noreply.github.com>
2021-01-24 20:42:38 -0500

    Update HeatMap.R
    cleaned up comments

--- scripts/HeatMap.R
+++ scripts/HeatMap.R
@@ -21,9 +21,6 @@ library("gplots")
 seqs <- read.alignment("analysis/MACSE2_Out_AA.fas", format = "fasta", whole.header = TRUE)
 
 ## Process the sequence names
-#seqs$nam <- n1[,1]
-#names = seqs$nam
-seqs$nam
 for (i in 1:length(seqs$nam)) {
   val = seqs$nam[i]
   print(val)
@@ -40,73 +37,13 @@ for (i in 1:length(seqs$nam)) {
   seqs$nam[i] = paste(first, second)
 }
 
-
-#for (val in seqs$nam) {
-#for (i in 1:length(seqs$nam)) {
-#  first = strsplit(val, " ")[[1]][2]
-#  second = strsplit(val, " ")[[1]][3]
-  
-#  if (first == "PREDICTED:") 
-#  {
-#    first = strsplit(val, " ")[[1]][3]
-#    second = strsplit(val, " ")[[1]][4]
-#  }
-  #print(c(strsplit(val, " ")[[1]][2], strsplit(val, " ")[[1]][3]) )
-  #print(paste(first, second))
-#  seqs$nam[i] = paste(first, second)
-#}
-
-#seqs$nam <- 
-
-#x = strsplit(names, " ")
-#seqs$nam <- n1[,1]
-#x[1:2]
-
+## Do matrix calculations
 aa <- dist.alignment(seqs, matrix = "similarity")
+
+## Convert to R matrix type, will be used for plotting
 aa1 <- as.matrix(aa)
 
-#aa1
-#range(aa1) 
-
-
-#heatmap(aa1, cexRow = 0.5, cexCol = 0.5)
-# Use 'scale' to normalize
-#heatmap(data, scale="column")
-#heatmap(aa1, scale="column")
-
-#heatmap(aa1, cexRow = 0.5, cexCol = 0.5, Rowv = NA, Colv = NA)
-
-#aa2 <- aa1[1:7, 8:12]
-#heatmap(aa2, cexRow = 0.5, cexCol = 0.5)
-
-#https://www.datanovia.com/en/lessons/heatmap-in-r-static-and-interactive-visualization/
-
-# Use RColorBrewer color palette names
-#library("RColorBrewer")
-#col <- colorRampPalette(brewer.pal(10, "RdYlBu"))(350)
-#heatmap(aa1, scale = "none", col =  col, 
-#        RowSideColors = rep(c("blue", "pink"), each = 16),
-#        ColSideColors = c(rep("purple", 5), rep("orange", 6)))
-
-#install.packages("gplots")
-#library("gplots")
-
-#heatmap.2(aa1, scale = "none", col = bluered(400), 
-#          trace = "none", density.info = "none")
-
-
 ## Plotting
-
-#heatmap.2(aa1, scale = "none", col = bluered(400), cexRow = 0.5, cexCol = 0.5,
-#          trace = "none", density.info = "none", reorderfun=function(d, w) reorder(d, w, agglo.FUN = mean) )
-
-#heatmap.2(x=aa1, 
-#          dendrogram="row",
-#          scale="none",
-#          col="bluered",
-#          trace="none",
-#          labCol=FALSE)
-
 # Default for now
 # https://www.rdocumentation.org/packages/gplots/versions/3.1.1/topics/heatmap.2
 # https://biocorecrg.github.io/CRG_RIntroduction/heatmap-2-function-from-gplots-package.html
@@ -121,74 +58,11 @@ heatmap.2(x=aa1,
           labCol=FALSE,
           cexRow=1,cexCol=1,margins=c(12,8))
 
-# END OF FILE
-
-
-
+## END OF FILE
 
 exit <- function() {
   .Internal(.invokeRestart(list(NULL, NULL), NULL))
 }
 exit()
 
-# #########################################################################################
-# Codon Alignment
-# #########################################################################################
-# https://www.rdocumentation.org/packages/gplots/versions/3.0.4/topics/heatmap.2
-library(seqinr)
-seqs <- read.alignment("MACSE2_Out_NT.fas", format = "fasta")
-
-aa <- dist.alignment(seqs, matrix = "similarity")
-aa1 <- as.matrix(aa)
-library("gplots")
-#heatmap.2(aa1, scale = "none", col = bluered(100), 
-#          trace = "none", density.info = "none")
-heatmap.2(aa1, scale = "none", col = bluered(100), cexRow = 0.5, cexCol = 0.5,
-          trace = "none", density.info = "none")
-
-heatmap.2(aa1, scale = "none", col = bluered(100), cexRow = 0.5, cexCol = 0.5,
-          trace = "none", density.info = "none", reorderfun=function(d, w) reorder(d, w, agglo.FUN = mean) )
-# #########################################################################################
-
-
-
-# #########################################################################################
-# Protein MSA
-# #########################################################################################
-protein_seqs <- read.alignment("MACSE2_Out_AA.msa", format = "fasta")
-
-protein_aa <- dist.alignment(protein_seqs, matrix = "similarity")
-protein_aa1 <- as.matrix(protein_aa)
-library("gplots")
-
-heatmap.2(protein_aa1, scale = "none", col = bluered(100), 
-          trace = "none", density.info = "none")
-heatmap.2(protein_aa1, scale = "none", col = bluered(100), cexRow = 0.5, cexCol = 0.5,
-          trace = "none", density.info = "none")
-
-heatmap.2(protein_aa1, scale = "none", col = bluered(100), cexRow = 0.5, cexCol = 0.5,
-          trace = "none", density.info = "none", reorderfun=function(d, w) reorder(d, w, agglo.FUN = mean) )
-
-heatmap.2(aa1, scale = "none", col = bluered(400), cexRow = 0.5, cexCol = 0.5,
-          trace = "none", density.info = "none", reorderfun=function(d, w) reorder(d, w, agglo.FUN = mean) )
-
-
-# #########################################################################################
-
-
-library("d3heatmap")
-d3heatmap(scale(aa1), colors = "RdYlBu",
-          k_row = 4, # Number of groups in rows
-          k_col = 2 # Number of groups in columns
-)
-
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-BiocManager::install("ComplexHeatmap")
-
-
-library("RColorBrewer")
-heatmap.2(data_matrix,col=brewer.pal(11,"RdBu"),scale="row", trace="none")
-
-
-# https://davetang.org/muse/2010/12/06/making-a-heatmap-with-r/
+ 
